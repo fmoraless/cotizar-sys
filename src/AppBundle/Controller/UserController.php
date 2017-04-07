dbdbd60d9ee6d390ccf02f89d8ac783389804a38
Francisco Morales Sánchez <franticzero@gmail.com>
2017-04-07 17:24:04 -0300

    Formulario de Registro.

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -6,10 +6,23 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use BackendBundle\Entity\User;
+use AppBundle\Form\RegisterType;
+
 class UserController extends Controller{
     
     public function loginAction(Request $request){
         
         return $this->render('AppBundle:User:login.html.twig');
+    }
+	
+	public function registerAction(Request $request){
+        
+		$user = new User();
+		$form = $this->createForm(RegisterType::class, $user);
+		
+        return $this->render('AppBundle:User:register.html.twig', array(
+			"form" => $form->createView()
+		));
     }
 }
