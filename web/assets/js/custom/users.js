802d664e01a68ff3b25a421b052df3ea02b828e4
Claudia <Claudia@Claudia-PC>
2017-04-07 01:02:31 -0300

    Incluyendo Jquery y Bootstrap

---
+++ web/assets/js/custom/users.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+//	alert("USERS");
+	var ias = jQuery.ias({
+		container: '.box-users',
+		item: '.user-item',
+		pagination: '.pagination',
+		next: '.pagination .next_link',
+		triggetPageThreshold: 5
+	});
+	
+	ias.extension(new IASTriggerExtension({
+		text: 'Ver más personas',
+		offset: 3
+	}));
+	
+	ias.extension(new IASSpinnerExtension({
+		src: URL+'/../assets/images/ajax-loader.gif'
+	}));
+	
+	ias.extension(new IASNoneLeftExtension({
+		text: 'No hay mas personas'
+	}));
+	
+	ias.on('ready', function(event){
+		followButtons();
+	});
+	ias.on('rendered', function(event){
+		followButtons();
+	});
+});
